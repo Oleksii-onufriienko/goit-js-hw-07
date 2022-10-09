8e2918ee9dd1babb07f2fe9470537fe845379958
Oleksii Onufriienko <o.alexey1202@gmail.com>
2022-10-09 22:45:20 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refGallery = document.querySelector(".gallery");
+let modalWindow = null;
+
+const arrayItem = [];
+let strWhisAllItem = ``;
+
+galleryItems.forEach((element) => {
+  const { preview, description } = element;
+  strWhisAllItem += `<img class="gallery__image gallery__link gallery__item" src="${preview}" alt="${description}" />`;
+  return;
+});
+
+refGallery.insertAdjacentHTML("afterbegin", strWhisAllItem);
+
+refGallery.addEventListener("click", () => {
+  const { original } = galleryItems.find(
+    (element) => element.description === event.target.alt
+  );
+
+  modalWindow = basicLightbox.create(`<img src="${original}">`);
+
+  modalWindow.show();
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === `Escape` && modalWindow.visible()) {
+    modalWindow.close();
+  }
+});
