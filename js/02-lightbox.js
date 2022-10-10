2bb029c7a4f77371d1ada4a10b0b93ff5cd025ad
Oleksii Onufriienko <o.alexey1202@gmail.com>
2022-10-10 16:38:26 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const refGallery = document.querySelector(".gallery");
+let modalWindow = null;
+
+let strWhisAllItem = ``;
+
+galleryItems.forEach((element) => {
+  const { preview, original, description } = element;
+
+  strWhisAllItem += `<a class="gallery__item" href="${original}" onclick="return false;">
+  <img class="gallery__image" src="${preview}" alt="${description}" title =${description}/>
+</a>`;
+  return;
+});
+
+refGallery.insertAdjacentHTML("afterbegin", strWhisAllItem);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+  captionDelay: 250,
+});
